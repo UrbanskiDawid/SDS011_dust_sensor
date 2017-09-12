ca71b7731b12f210391853ecbf2696a087beefd4
Dawid Urbanski <git@dawidurbanski.pl>
2017-09-12 17:21:19 -0400

    class

--- SDS011.py
+++ SDS011.py
@@ -1,8 +1,47 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 import serial, time, struct, array
 from datetime import datetime
+
+
+
+class ReadingMessage:
+
+    def __init__(self, data):
+
+      self.date = datetime.now()
+
+      s = struct.unpack('<cchhHcc', data) # Decode the packet - big endian, byte head0, byte head1, short for pm2.5, short for pm10, short: sensor ID, checksum, message tail
+
+      self.raw = data[:]
+
+      assert s[0]==b'\xAA'
+      assert s[1]==b'\xC0'
+
+      self.pm_25 = s[2]/10.0
+
+      self.pm_10 = s[3]/10.0
+
+      self.sensorID = s[4]
+
+      #checksum
+      self.checksum = int.from_bytes(s[5], byteorder='big', signed=False)
+      cs = 0
+      for i in range(2, len(data)-2):
+        cs += data[i]
+      cs = cs  % 256
+      assert cs == self.checksum
+
+      assert s[6], b'\xAB' #message tail
+
+    def __str__(self):
+      return "{}: [{}] SENSOR_ID: {} PM 2.5: {} μg/m^3  PM 10: {} μg/m^3".format(self.date.strftime("%d %b %Y %H:%M:%S.%f"),
+                                                                                   ' '.join('{:02x}'.format(x) for x in self.raw),
+                                                                                   self.sensorID,
+                                                                                   self.pm_25,
+                                                                                   self.pm_10)
+
+
 
 ser = serial.Serial()
 ser.port = "/dev/ttyUSB0" # Set this to your serial port
@@ -11,25 +50,12 @@ ser.baudrate = 9600
 ser.open()
 ser.flushInput()
 
-byte, lastbyte = "\x00", "\x00"
-cnt = 0
+byte = lastbyte = b'\x00'
+
 while True:
     lastbyte = byte
     byte = ser.read(size=1)
-    print("{:3} {}".format(ord(byte),byte))
-#    print("Got byte %x" %ord(byte))
-    # We got a valid packet header
-    if lastbyte == b'\xaa' and byte == b'\xC0':
+    if lastbyte == b'\xAA' and byte == b'\xC0':
         sentence = ser.read(size=8) # Read 8 more bytes
-        readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, short for pm2.5, short for pm10, 2 bytes: sensor ID, checksum, message tail
-        print( array.array('B',sentence) )
-        pm_25 = readings[0]/10.0
-        pm_10 = readings[1]/10.0
-        # ignoring the checksum and message tail
-        b'\xAB'
-        if (cnt == 0 ):
-            line = "PM 2.5: {} μg/m^3  PM 10: {} μg/m^3".format(pm_25, pm_10)
-            print(datetime.now().strftime("%d %b %Y %H:%M:%S.%f: ")+line)
-        cnt += 1
-        if (cnt == 5):
-            cnt = 0
+        r = ReadingMessage(b'\xAA\xC0'+sentence)
+        print(r)
